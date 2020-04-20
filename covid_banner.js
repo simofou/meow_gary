d5789269bf5804c3b7553f6fe0d687dd51abe64c
simon foucher <simon@fond.co>
2020-04-20 15:33:50 -0700

    adapt dismissibleItem function

--- covid_banner.js
+++ covid_banner.js
@@ -1,50 +1,43 @@
-;(function(window){
+//Js function to add dynamic COVID banner
 
-  var dismissibleItem = function(el, type, value) {
+var dismissibleItem = function(el, value) {
 
-    var html = '<span>' + value + ' <button type="button" class="close">X</button></span>';
-    
-    el.removeAttribute('data-component');
-    el.removeAttribute('data-value');
-    el.removeAttribute('data-type');
+  var html = '<span>' + value + ' <button type="button" class="close">X</button></span>';
 
-    el.classList.add('dismissible', 'dismissible-' + type);
+  el.removeAttribute('data-component');
+  el.removeAttribute('data-value');
 
-    el.innerHTML = html;
+  el.classList.add('dismissible');
 
-    el.querySelector('.close').addEventListener('click', function(event){
-      var height = el.offsetHeight,
-        opacity = 1,
+  el.innerHTML = html;
+
+  el.querySelector('.close').addEventListener('click', function(event){
+    var height = el.offsetHeight,
+      opacity = 1,
+      timeout = null;
+    function reduceHeight() {
+      height -= 2;
+      el.setAttribute('style', 'height: ' + height + 'px; opacity: ' + opacity);
+      if (height <= 0) {
+        window.clearInterval(timeout) ;
         timeout = null;
-      function reduceHeight() {
-        height -= 2;
-        el.setAttribute('style', 'height: ' + height + 'px; opacity: ' + opacity);
-        if (height <= 0) {
-          window.clearInterval(timeout) ;
-          timeout = null;
-          el.remove();
-        }
+        el.remove();
       }
-      function fade() {
-        opacity -= .1;
-        el.setAttribute('style', 'opacity: ' + opacity);
-        if (opacity <= 0) {
-          window.clearInterval(timeout);
-          timeout = window.setInterval(reduceHeight, 1);
-        }
-      }
-      timeout = window.setInterval(fade, 25);
-    });
-
-  };
-
-  var dismissibles = Array.prototype.slice.call(document.querySelectorAll('[data-component="dismissible-item"]'));
-  if (dismissibles.length) {
-    for (var i = 0; i < dismissibles.length; i++) {
-      var type = dismissibles[i].getAttribute('data-type'),
-        value = dismissibles[i].getAttribute('data-value');
-      new dismissibleItem(dismissibles[i], type, value)
     }
-  }
+    function fade() {
+      opacity -= .1;
+      el.setAttribute('style', 'opacity: ' + opacity);
+      if (opacity <= 0) {
+        window.clearInterval(timeout);
+        timeout = window.setInterval(reduceHeight, 1);
+      }
+    }
+    timeout = window.setInterval(fade, 25);
+  });
 
-})(window);
+};
+
+var dismissible = document.querySelector('[data-component="dismissible-item"]')
+    value = dismissible.getAttribute('data-value');
+
+new dismissibleItem(dismissible, value)
